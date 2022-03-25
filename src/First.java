91012d619986dea07a455aab5a6d01d825865d62
NasiemJamshidi <nasiem_jamshidi@hotmail.com>
2022-03-25 03:44:51 +0100

    origin

---
+++ src/First.java
@@ -0,0 +1,48 @@
+public class First {
+        public static void main(String[] args) {
+           System.out.println("Hello Nasiem jamshidi!");
+
+            String name= "Nasiem!";
+            System.out.println( "Hello" + name);
+            person();
+
+            String[] names = new String[5];
+
+            names[0] = "Ahmad";
+            names[1] = "koko";
+            names[2] = "lolo";
+            names[3] = "bako";
+            names[4] = "kako";
+            System.out.println(names [3]);
+            System.out.println( "hallo " + names [0]);
+            System.out.println( "hallo " + names [1]);
+            System.out.println( "hallo " + names [2]);
+            System.out.println( "hallo " + names [3]);
+            System.out.println( "hallo " + names [4]);
+        }
+
+    private static void person() {
+
+
+        String name = "kiko";
+        int age = 37 ;
+        String City = "Kabul";
+
+        System.out.println( "hello"+ name);
+        System.out.println( "your age is "  + age + " years old");
+        System.out.println( "and you are living in " +    City);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
